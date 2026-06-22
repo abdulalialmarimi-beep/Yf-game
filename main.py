64bbf30ffa0b41bc000da2e17dfbc36ed30651db
abdulalialmarimi-beep <abdulalialmarimi@gmail.com>
2026-06-22 17:54:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ from config import TOKEN, PREFIX
 import threading
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
-print("TEST 123")
-
 subprocess.run(["pip", "install", "motor"], check=True)
 
 class Handler(BaseHTTPRequestHandler):
@@ -15,51 +13,36 @@ class Handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b'Bot is running!')
-
     def log_message(self, *args):
         pass
 
 def run_server():
-    server = HTTPServer(("0.0.0.0", 8080), Handler)
+    server = HTTPServer(('0.0.0.0', 8080), Handler)
     server.serve_forever()
 
 intents = discord.Intents.default()
 intents.message_content = True
-
-bot = commands.Bot(
-    command_prefix=PREFIX,
-    intents=intents
-)
+bot = commands.Bot(command_prefix=PREFIX, intents=intents)
 
 @bot.event
 async def on_ready():
     print(f"✅ البوت شغال الآن: {bot.user}")
 
 async def load_cogs():
-    cogs = [
-        "cogs.test_cog",
-        "cogs.hisab",
-        "cogs.jam3",
-        "cogs.mufrad",
-        "cogs.kat",
-        "cogs.as3ar",
-        "cogs.rabt",
-        "cogs.aks",
-        "cogs.fak",
-        "cogs.s7_5ata",
-        "cogs.niqat"
-    ]
-
-    for cog in cogs:
-        try:
-            await bot.load_extension(cog)
-            print(f"✅ Loaded: {cog}")
-        except Exception as e:
-            print(f"❌ Error loading {cog}: {e}")
+    await bot.load_extension("cogs.test_cog")
+    await bot.load_extension("cogs.hisab")
+    await bot.load_extension("cogs.jam3")
+    await bot.load_extension("cogs.mufrad")
+    await bot.load_extension("cogs.kat")
+    await bot.load_extension("cogs.as3ar")
+    await bot.load_extension("cogs.rabt")
+    await bot.load_extension("cogs.aks")
+    await bot.load_extension("cogs.fak")
+    await bot.load_extension("cogs.s7_5ata")
+    await bot.load_extension("cogs.niqat")
 
 async def main():
     threading.Thread(target=run_server, daemon=True).start()
-
     async with bot:
         await load_cogs()
         await bot.start(TOKEN)
